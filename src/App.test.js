57982e10c98e946ef8f36827bea024fb677e604a
Ignacio Capodanno <icapodanno@gmail.com>
2021-10-21 03:49:37 -0300

    feat: adding test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders four blocks per page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let blocksNumber  = screen.getAllByAltText('Tennis Shoe');
+  expect(blocksNumber.length).toEqual(4);
+});
+
+test('only visible next button on first page', () => {
+  render(<App />);
+  let element = screen.getAllByRole('button');
+  expect(element.length).toEqual(1);
+  expect(element[0]).toHaveClass('rightArrow')
+});
+
+test('only visible prev button on last page', () => {
+  render(<App />);
+  
+  // moving to second page clicking on right arrow
+  userEvent.click(screen.getByRole('button'))
+  // moving to last page clicking on right arrow
+  let rightArrow = screen.getAllByRole('button').find(value => value.className === 'rightArrow')
+  userEvent.click(rightArrow)
+  
+  let element = screen.getAllByRole('button');
+  expect(element.length).toEqual(1);
+  expect(element[0]).toHaveClass('leftArrow')
 });
